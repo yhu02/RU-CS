1f0da4e5bcaa6d67cf47a124b5cf451981b63e32
Yvo Hu <yvo.hu@alliander.com>
2024-02-07 16:57:23 +0100

    natural computing init

---
+++ year1/natural-computing/assignment-1/artistoo/examples/build-examples/write-example-node.bash
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+examplename=$1
+
+templatefile=simulation-files/$examplename.js
+
+
+echo 'let CPM = require("../../build/artistoo-cjs.js")'
+echo -e '\n'
+awk -f extract-require.awk $templatefile | grep "#node" | awk -F "::" '{gsub(/ /, "", $2); print "let" $3 " = require( \"" $2 "\" ) "}'
+
+sed -e '1,/START CLASS DEFINITION/d' -e '/END CLASS DEFINITION/,$d' $templatefile
+
+sed -e '1,/START CONFIGURATION/d' -e '/END CONFIGURATION/,$d' $templatefile
+echo -e '\n'
+
+if [[ $( cat $templatefile | grep "START METHODS OBJECT" | wc -l) -eq 0 ]] ; then 
+	echo -e '\tlet sim = new CPM.Simulation( config, {} )'
+else
+	sed -e '1,/START METHODS OBJECT/d' -e '/END METHODS OBJECT/,$d' $templatefile | \
+		awk '{print "\t",$0}'
+	echo -e '\tlet sim = new CPM.Simulation( config, custommethods )'
+fi
+sed -e '1,/START ADDCONSTRAINTS/d' -e '/END ADDCONSTRAINTS/,$d' $templatefile
+echo -e "\n"
+
+
+echo -e '\n'
+
+sed -e '1,/START METHODS DEFINITION/d' -e '/END METHODS DEFINITION/,$d' $templatefile
+echo -e '\n'
+
+echo "sim.run()"
