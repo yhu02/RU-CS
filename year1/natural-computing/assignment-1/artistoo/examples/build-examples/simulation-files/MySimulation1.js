427b6c80fceef3093a65406d10ef92eb39d687fa
Yvo Hu <yvo.hu@alliander.com>
2024-02-10 21:57:41 +0100

    update

--- year1/natural-computing/assignment-1/artistoo/examples/build-examples/simulation-files/MySimulation1.js
+++ year1/natural-computing/assignment-1/artistoo/examples/build-examples/simulation-files/MySimulation1.js
@@ -1,6 +1,6 @@
 /* globals CPM */
 
-const { CPM } = require("../../html/artistoo")
+const { CPM } = require("../../html/artistoo");
 
 /* 	================= DESCRIPTION ===================== */
 /* This text is printed on the HTML page. */
@@ -13,10 +13,10 @@ const { CPM } = require("../../html/artistoo")
 /* 	The following functions are defined below and will be added to
 	the simulation object.*/
 let custommethods = {
-	initializeGrid : initializeGrid,
-	buildObject : buildObject,
-	drawBelow : drawBelow
-}
+  initializeGrid: initializeGrid,
+  buildObject: buildObject,
+  drawBelow: drawBelow,
+};
 
 /* END METHODS OBJECT Do not remove this line */
 
@@ -27,50 +27,49 @@ let custommethods = {
 /* The following custom methods will be added to the simulation object
 below. */
 
-function drawBelow(){
-	this.Cim.drawPixelSet( this.object)
+function drawBelow() {
+  this.Cim.drawPixelSet(this.object);
 }
 
-function initializeGrid(){
-	
-	// add the initializer if not already there
-	if( !this.helpClasses["gm"] ){ this.addGridManipulator() }
-	
-	let nrcells = this.conf["NRCELLS"], cellkind, i
-	this.buildObject()
-		
+function initializeGrid() {
+  // add the initializer if not already there
+  if (!this.helpClasses["gm"]) {
+    this.addGridManipulator();
+  }
 
-	// Seed the right number of cells for each cellkind
-	for( cellkind = 0; cellkind < nrcells.length; cellkind ++ ){
-			
-		for( i = 0; i < nrcells[cellkind]; i++ ){
-			// first cell always at the midpoint. Any other cells
-			// randomly.				
-			if( i == 0 ){
-				this.gm.seedCellAt( cellkind+1, this.C.midpoint )
-			} else {
-				this.gm.seedCell( cellkind+1 )
-			}
-		}
-	}
-	
-}
-	
-function buildObject(){
-	let gridHeight = this.C.extents[1]
-	let gridWidth = this.C.extents[0]
+  let nrcells = this.conf["NRCELLS"],
+    cellkind,
+    i;
+  this.buildObject();
 
-	this.object = this.gm.makeCircle( [gridHeight/2,gridWidth/2], 50 )
-	this.gm.assignCellPixels( this.object, 2)
-	this.C.add( new CPM.BorderConstraint({
-		BARRIER_VOXELS : this.object
-	}) )	
+  // Seed the right number of cells for each cellkind
+  for (cellkind = 0; cellkind < nrcells.length; cellkind++) {
+    for (i = 0; i < nrcells[cellkind]; i++) {
+      // first cell always at the midpoint. Any other cells
+      // randomly.
+      if (i == 0) {
+        this.gm.seedCellAt(cellkind + 1, this.C.midpoint);
+      } else {
+        this.gm.seedCell(cellkind + 1);
+      }
+    }
+  }
 }
 
+function buildObject() {
+  let gridHeight = this.C.extents[1];
+  let gridWidth = this.C.extents[0];
+
+  this.object = this.gm.makeCircle([gridHeight / 2, gridWidth / 2], 50);
+  this.gm.assignCellPixels(this.object, 2);
+  this.C.add(
+    new CPM.BorderConstraint({
+      BARRIER_VOXELS: this.object,
+    })
+  );
+}
 
 /* END METHODS DEFINITION Do not remove this line */
-
-
 
 /* ================= CONFIGURATION ===================== */
 
@@ -80,73 +79,73 @@ function buildObject(){
 	----------------------------------
 */
 let config = {
+  // Grid settings
+  ndim: 2,
+  field_size: [250, 250],
 
-	// Grid settings
-	ndim : 2,
-	field_size : [250,250],
-	
-	// CPM parameters and configuration
-	conf : {
-		// Basic CPM parameters
-		torus : [true,true],						// Should the grid have linked borders?
-		seed : 1,							// Seed for random number generation.
-		T : 20,								// CPM temperature
-		
-		// Constraint parameters. 
-		// Mostly these have the format of an array in which each element specifies the
-		// parameter value for one of the cellkinds on the grid.
-		// First value is always cellkind 0 (the background) and is often not used.
+  // CPM parameters and configuration
+  conf: {
+    // Basic CPM parameters
+    torus: [true, true], // Should the grid have linked borders?
+    seed: 1, // Seed for random number generation.
+    T: 20, // CPM temperature
 
-		
-		// Adhesion parameters:
-		J : [ [0,20, 0], [20,0, 0], [0,0,0] ],
-		
-		// VolumeConstraint parameters
-		LAMBDA_V : [0,50, 50],				// VolumeConstraint importance per cellkind
-		V : [0,200, 0],					// Target volume of each cellkind
-		
-		// PerimeterConstraint parameters
-		LAMBDA_P : [0,2,0],				// PerimeterConstraint importance per cellkind
-		P : [0,180,0],					// Target perimeter of each cellkind
-		
-		// ActivityConstraint parameters
-		LAMBDA_ACT : [0,200,0],			// ActivityConstraint importance per cellkind
-		MAX_ACT : [0,20,0],				// Activity memory duration per cellkind
-		ACT_MEAN : "geometric"				// Is neighborhood activity computed as a
-		// "geometric" or "arithmetic" mean?
-	},
-	
-	// Simulation setup and configuration: this controls stuff like grid initialization,
-	// runtime, and what the output should look like.
-	simsettings : {
-	
-		// Cells on the grid
-		NRCELLS : [0,0, 0],					// Number of cells to seed for all
-		// non-background cellkinds.
-		// Runtime etc
-		BURNIN : 50,
-		RUNTIME : 1000,
-		RUNTIME_BROWSER : "Inf",
-		
-		// Visualization
-		CANVASCOLOR : "eaecef",
-		CELLCOLOR : ["000000"],
-		ACTCOLOR : [true],			// Should pixel activity values be displayed?
-		SHOWBORDERS : [false],				// Should cellborders be displayed?
-		zoom : 2,							// zoom in on canvas with this factor.
-		
-		// Output images
-		SAVEIMG : true,						// Should a png image of the grid be saved
-		// during the simulation?
-		IMGFRAMERATE : 1,					// If so, do this every <IMGFRAMERATE> MCS.
-		SAVEPATH : "output/img/MySimulation3",	// ... And save the image in this folder.
-		EXPNAME : "MySimulation3",					// Used for the filename of output images.
-		
-		// Output stats etc
-		STATSOUT : { browser: false, node: true }, // Should stats be computed?
-		LOGRATE : 10							// Output stats every <LOGRATE> MCS.
+    // Constraint parameters.
+    // Mostly these have the format of an array in which each element specifies the
+    // parameter value for one of the cellkinds on the grid.
+    // First value is always cellkind 0 (the background) and is often not used.
 
-	}
-}
+    // Adhesion parameters:
+    J: [
+      [0, 20, 0],
+      [20, 0, 0],
+      [0, 0, 0],
+    ],
+
+    // VolumeConstraint parameters
+    LAMBDA_V: [0, 50, 50], // VolumeConstraint importance per cellkind
+    V: [0, 200, 0], // Target volume of each cellkind
+
+    // PerimeterConstraint parameters
+    LAMBDA_P: [0, 2, 0], // PerimeterConstraint importance per cellkind
+    P: [0, 180, 0], // Target perimeter of each cellkind
+
+    // ActivityConstraint parameters
+    LAMBDA_ACT: [0, 200, 0], // ActivityConstraint importance per cellkind
+    MAX_ACT: [0, 20, 0], // Activity memory duration per cellkind
+    ACT_MEAN: "geometric", // Is neighborhood activity computed as a
+    // "geometric" or "arithmetic" mean?
+  },
+
+  // Simulation setup and configuration: this controls stuff like grid initialization,
+  // runtime, and what the output should look like.
+  simsettings: {
+    // Cells on the grid
+    NRCELLS: [0, 0, 0], // Number of cells to seed for all
+    // non-background cellkinds.
+    // Runtime etc
+    BURNIN: 50,
+    RUNTIME: 1000,
+    RUNTIME_BROWSER: "Inf",
+
+    // Visualization
+    CANVASCOLOR: "eaecef",
+    CELLCOLOR: ["000000"],
+    ACTCOLOR: [true], // Should pixel activity values be displayed?
+    SHOWBORDERS: [false], // Should cellborders be displayed?
+    zoom: 2, // zoom in on canvas with this factor.
+
+    // Output images
+    SAVEIMG: true, // Should a png image of the grid be saved
+    // during the simulation?
+    IMGFRAMERATE: 1, // If so, do this every <IMGFRAMERATE> MCS.
+    SAVEPATH: "output/img/MySimulation3", // ... And save the image in this folder.
+    EXPNAME: "MySimulation3", // Used for the filename of output images.
+
+    // Output stats etc
+    STATSOUT: { browser: false, node: true }, // Should stats be computed?
+    LOGRATE: 10, // Output stats every <LOGRATE> MCS.
+  },
+};
 /*	---------------------------------- */
 /* Do not remove this line: END CONFIGURATION */
--- year1/natural-computing/assignment-1/artistoo/examples/build-examples/simulation-files/MySimulation2.js
+++ year1/natural-computing/assignment-1/artistoo/examples/build-examples/simulation-files/MySimulation2.js
@@ -7,8 +7,6 @@
 
 	**/
 
-
-
 /* ================= ADD MORE CONSTRAINTS ===================== */
 
 /* Example of how to add a constraint. Put this code between the
@@ -26,55 +24,50 @@ sim.C.add( pconstraint ) */
 
 /* END ADDCONSTRAINTS Do not remove this line */
 
-
-
 /* ================= WRITE CUSTOM METHODS ===================== */
 
 /* START METHODS DEFINITION Do not remove this line */
 
 /* The following custom methods are used for control buttons on the html page.*/
 
-function startsim(){
-	if( !sim.running ){
-		sim.running = true
-	}
+function startsim() {
+  if (!sim.running) {
+    sim.running = true;
+  }
 }
-function stopsim(){
-	sim.running = false
+function stopsim() {
+  sim.running = false;
 }
-function seedCell( k ){
-	sim.gm.seedCell(k)
+function seedCell(k) {
+  sim.gm.seedCell(k);
 }
-function seedCells( ncells ){
-	for( let i = 0; i < ncells; i++ ){
-		seedCell( 1 )
-	}
+function seedCells(ncells) {
+  for (let i = 0; i < ncells; i++) {
+    seedCell(1);
+  }
 }
-function killCell(){
-	let t
-	let cells = Object.keys( sim.C.getStat( CPM.PixelsByCell ) )
-	if( cells.length > 0 ){
-		t = cells.pop()
-		for( let cp of sim.C.cellPixels() ){
-			if( cp[1] == t ){
-				sim.C.setpix( cp[0], 0 )
-			}
-		}
-	}
-	sim.C.stat_values = {}
-
+function killCell() {
+  let t;
+  let cells = Object.keys(sim.C.getStat(CPM.PixelsByCell));
+  if (cells.length > 0) {
+    t = cells.pop();
+    for (let cp of sim.C.cellPixels()) {
+      if (cp[1] == t) {
+        sim.C.setpix(cp[0], 0);
+      }
+    }
+  }
+  sim.C.stat_values = {};
 }
-function killAllCells(){
-	let cells = Object.keys( sim.C.getStat( CPM.PixelsByCell ) )
-	if( cells.length == 0 ) return
-	for( let cp of sim.C.cellPixels() ){
-		sim.C.setpix( cp[0], 0 )
-	}
+function killAllCells() {
+  let cells = Object.keys(sim.C.getStat(CPM.PixelsByCell));
+  if (cells.length == 0) return;
+  for (let cp of sim.C.cellPixels()) {
+    sim.C.setpix(cp[0], 0);
+  }
 }
 
 /* END METHODS DEFINITION Do not remove this line */
-
-
 
 /* ================= CONFIGURATION ===================== */
 
@@ -84,73 +77,73 @@ function killAllCells(){
 	----------------------------------
 */
 let config = {
+  // Grid settings
+  ndim: 2,
+  field_size: [250, 250],
 
-	// Grid settings
-	ndim : 2,
-	field_size : [250,250],
-	
-	// CPM parameters and configuration
-	conf : {
-		// Basic CPM parameters
-		torus : [true,true],						// Should the grid have linked borders?
-		seed : 1,							// Seed for random number generation.
-		T : 20,								// CPM temperature
-		
-		// Constraint parameters. 
-		// Mostly these have the format of an array in which each element specifies the
-		// parameter value for one of the cellkinds on the grid.
-		// First value is always cellkind 0 (the background) and is often not used.
+  // CPM parameters and configuration
+  conf: {
+    // Basic CPM parameters
+    torus: [true, true], // Should the grid have linked borders?
+    seed: 1, // Seed for random number generation.
+    T: 20, // CPM temperature
 
-		
-		// Adhesion parameters:
-		J : [ [0,20, 0], [20,0, 0], [0,0,0] ],
-		
-		// VolumeConstraint parameters
-		LAMBDA_V : [0,50, 50],				// VolumeConstraint importance per cellkind
-		V : [0,200, 0],					// Target volume of each cellkind
-		
-		// PerimeterConstraint parameters
-		LAMBDA_P : [0,2,0],				// PerimeterConstraint importance per cellkind
-		P : [0,180,0],					// Target perimeter of each cellkind
-		
-		// ActivityConstraint parameters
-		LAMBDA_ACT : [0,200,0],			// ActivityConstraint importance per cellkind
-		MAX_ACT : [0,20,0],				// Activity memory duration per cellkind
-		ACT_MEAN : "geometric"				// Is neighborhood activity computed as a
-		// "geometric" or "arithmetic" mean?
-	},
-	
-	// Simulation setup and configuration: this controls stuff like grid initialization,
-	// runtime, and what the output should look like.
-	simsettings : {
-	
-		// Cells on the grid
-		NRCELLS : [50,0, 0],					// Number of cells to seed for all
-		// non-background cellkinds.
-		// Runtime etc
-		BURNIN : 50,
-		RUNTIME : 1000,
-		RUNTIME_BROWSER : "Inf",
-		
-		// Visualization
-		CANVASCOLOR : "eaecef",
-		CELLCOLOR : ["000000"],
-		ACTCOLOR : [true],			// Should pixel activity values be displayed?
-		SHOWBORDERS : [false],				// Should cellborders be displayed?
-		zoom : 2,							// zoom in on canvas with this factor.
-		
-		// Output images
-		SAVEIMG : true,						// Should a png image of the grid be saved
-		// during the simulation?
-		IMGFRAMERATE : 1,					// If so, do this every <IMGFRAMERATE> MCS.
-		SAVEPATH : "output/img/MySimulation3",	// ... And save the image in this folder.
-		EXPNAME : "MySimulation3",					// Used for the filename of output images.
-		
-		// Output stats etc
-		STATSOUT : { browser: false, node: true }, // Should stats be computed?
-		LOGRATE : 10							// Output stats every <LOGRATE> MCS.
+    // Constraint parameters.
+    // Mostly these have the format of an array in which each element specifies the
+    // parameter value for one of the cellkinds on the grid.
+    // First value is always cellkind 0 (the background) and is often not used.
 
-	}
-}
+    // Adhesion parameters:
+    J: [
+      [0, 20, 0],
+      [20, 0, 0],
+      [0, 0, 0],
+    ],
+
+    // VolumeConstraint parameters
+    LAMBDA_V: [0, 50, 50], // VolumeConstraint importance per cellkind
+    V: [0, 200, 0], // Target volume of each cellkind
+
+    // PerimeterConstraint parameters
+    LAMBDA_P: [0, 2, 0], // PerimeterConstraint importance per cellkind
+    P: [0, 180, 0], // Target perimeter of each cellkind
+
+    // ActivityConstraint parameters
+    LAMBDA_ACT: [0, 200, 0], // ActivityConstraint importance per cellkind
+    MAX_ACT: [0, 20, 0], // Activity memory duration per cellkind
+    ACT_MEAN: "geometric", // Is neighborhood activity computed as a
+    // "geometric" or "arithmetic" mean?
+  },
+
+  // Simulation setup and configuration: this controls stuff like grid initialization,
+  // runtime, and what the output should look like.
+  simsettings: {
+    // Cells on the grid
+    NRCELLS: [50, 0, 0], // Number of cells to seed for all
+    // non-background cellkinds.
+    // Runtime etc
+    BURNIN: 50,
+    RUNTIME: 1000,
+    RUNTIME_BROWSER: "Inf",
+
+    // Visualization
+    CANVASCOLOR: "eaecef",
+    CELLCOLOR: ["000000"],
+    ACTCOLOR: [true], // Should pixel activity values be displayed?
+    SHOWBORDERS: [false], // Should cellborders be displayed?
+    zoom: 2, // zoom in on canvas with this factor.
+
+    // Output images
+    SAVEIMG: true, // Should a png image of the grid be saved
+    // during the simulation?
+    IMGFRAMERATE: 1, // If so, do this every <IMGFRAMERATE> MCS.
+    SAVEPATH: "output/img/MySimulation3", // ... And save the image in this folder.
+    EXPNAME: "MySimulation3", // Used for the filename of output images.
+
+    // Output stats etc
+    STATSOUT: { browser: false, node: true }, // Should stats be computed?
+    LOGRATE: 10, // Output stats every <LOGRATE> MCS.
+  },
+};
 /*	---------------------------------- */
 /* Do not remove this line: END CONFIGURATION */
